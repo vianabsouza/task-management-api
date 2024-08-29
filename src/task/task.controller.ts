c37135eaeb5be9b8a53f3e3d641838c3e4648500
Barbara Viana <vianabsouza@gmail.com>
2024-08-28 22:01:23 -0300

    feat: create task service, implement create method, and integrate with controller

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { TaskDto } from './task.dto';
+import { TaskService } from './task.service';
 
 @Controller('task')
 export class TaskController {
+  constructor(private readonly taskService: TaskService) {}
+
   @Post()
   create(@Body() task: TaskDto) {
-    console.log(task);
+    this.taskService.create(task);
   }
 }
