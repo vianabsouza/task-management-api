cd70b3992f977d8e62ea17551da4e15f19aba14a
Barbara Viana <vianabsouza@gmail.com>
2024-10-17 04:27:39 -0300

    feat: add @httpcode decorator to sign in endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthResponseDto } from './auth.dto';
 import { AuthService } from './auth.service';
 
@@ -6,6 +6,7 @@ import { AuthService } from './auth.service';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @HttpCode(HttpStatus.OK)
   @Post('login')
   signIn(
     @Body('username') username: string,
