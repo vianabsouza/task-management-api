53ec521d616af0312e2c1e589e437e95677a283c
Barbara Viana <vianabsouza@gmail.com>
2024-10-17 04:02:40 -0300

    feat: add method to find user by username

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -12,4 +12,8 @@ export class UsersService {
     newUser.password = bcryptHasSync(newUser.password, 10);
     this.users.push(newUser);
   }
+
+  findByUserName(username: string): UserDto | null {
+    return this.users.find(user => user.username === username) || null;
+  }
 }
