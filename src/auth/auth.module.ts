0cb90d327aa61720a1f5862d994cba012b0ea116
Barbara Viana <vianabsouza@gmail.com>
2024-10-17 04:25:54 -0300

    feat: set up global jwt module with async configuration using ConfigService

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { UsersModule } from 'src/users/users.module';
 
-@Module({
+  @Module({
+    imports: [JwtModule.registerAsync({
+      global: true,
+      imports: [],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {expiresIn: +configService.get<string>('JWT_EXPIRATION_TIME')}
+      }),
+      inject: [ConfigService]
+    }), UsersModule],
   providers: [AuthService],
   controllers: [AuthController]
 })
