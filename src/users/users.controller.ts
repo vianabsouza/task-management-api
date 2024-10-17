161d4618d1cb203f11fb55f82dd2c45535a3c288
Barbara Viana <vianabsouza@gmail.com>
2024-10-17 03:20:37 -0300

    feat: add create method to users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UserDto } from './user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() user: UserDto) {
+    this.usersService.create(user)
+  }
+}
