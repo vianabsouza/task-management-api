f42e679e9025761709932f929f18cfe3af53a9e2
Barbara Viana <vianabsouza@gmail.com>
2024-10-17 04:24:33 -0300

    feat: implement authentication service with sign in method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+import { AuthResponseDto } from './auth.dto';
+import { compareSync as bcryptCompareSync } from 'bcrypt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  private jwtExpirationTimeInSecond: number
+
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService
+  ) {
+    this.jwtExpirationTimeInSecond = +this.configService.get<string>('JWT_EXPIRATION_TIME');
+  }
+
+  signIn(username: string, password: string): AuthResponseDto {
+    const foundUser = this.usersService.findByUserName(username)
+
+    if(!foundUser || !bcryptCompareSync(password, foundUser.password)) {
+      throw new UnauthorizedException();
+    }
+
+    const payload = {sub: foundUser.id, username : foundUser.username};
+
+    const token = this.jwtService.sign(payload);
+
+    return {token, expiresIn: this.jwtExpirationTimeInSecond}
+  }
+}
+
