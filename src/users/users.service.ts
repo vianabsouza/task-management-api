a4db8d5ec37832d48c49321c761139240791c8af
Barbara Viana <vianabsouza@gmail.com>
2024-10-17 03:21:48 -0300

    feat: implement user creation logic with id generation and password hashing

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { UserDto } from './user.dto';
+import {v4 as uuid} from 'uuid';
+import {hashSync as bcryptHasSync} from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users: UserDto[] = []
+
+  create(newUser: UserDto) {
+    newUser.id = uuid();
+    newUser.password = bcryptHasSync(newUser.password, 10);
+    this.users.push(newUser);
+  }
+}
