1269f4a8bd38e9b308930d390ca7ed1b47b6b4a2
Barbara Viana <vianabsouza@gmail.com>
2024-10-17 03:37:12 -0300

    build: install nestjs jwt as a dependency

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthResponseDto } from './auth.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  signIn(
+    @Body('username') username: string,
+    @Body('password') password: string
+  ): AuthResponseDto {
+    return this.authService.signIn(username, password);
+  }
+}
